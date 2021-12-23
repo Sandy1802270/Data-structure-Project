51851cdf71f4723bc566c43033da0bcf521946a9
Sandy1802270 <85247055+Sandy1802270@users.noreply.github.com>
2021-12-23 19:01:13 +0200

    Create minify.h

---
+++ minify.h
@@ -0,0 +1,11 @@
+#ifndef MINIFY_H
+#define MINIFY_H
+
+#include <bits/stdc++.h>
+/***************************************************************
+*******************Function Prototypes *************************
+****************************************************************/
+
+std::string Minify(std::ifstream& input);
+
+#endif // MINIFY_H
