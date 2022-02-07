cc8ca65aef77b1b6ddece06a9ff4f1fe54e52c51
Salma <85197391+salmaasherif@users.noreply.github.com>
2022-02-08 01:24:01 +0200

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,7 +7,7 @@
 #include <iostream>
 #include <fstream>
 #include <cstdio>
-
+#include <QDir>
 MainWindow::MainWindow(QWidget* parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -88,30 +88,23 @@ void MainWindow::on_pushButton_3_clicked()
     temp << input1;
     temp.close();
     std::ifstream inputFile("temp1.xml");
-
     std::ofstream temp3("middle.xml");
     std::ofstream outputFile("output.xml");
     temp3 << Minify2(inputFile, outputFile);
     temp3.close();
-
-
     std::ifstream temp2("middle.xml");
     XML_tree* t1 = new XML_tree();
     outputFile << Parser(temp2, outputFile);
     temp2.close();
-
-
     std::ifstream in2("output.xml");
     std::string output = Format(t1, in2, outputFile);
     QString s3 = QString::fromLocal8Bit(output.c_str());
-
     QFont font;
     font.setFamily("Courier");
     font.setStyleHint(QFont::Monospace);
     font.setFixedPitch(true);
     font.setPointSize(12);
     ui->plainTextEdit_2->setFont(font);
-
     QFontMetrics metrics(font);
     ui->plainTextEdit_2->setReadOnly(true);
     ui->plainTextEdit_2->setTabStopDistance(15);
@@ -128,31 +121,23 @@ void MainWindow::on_pushButton_6_clicked()
     temp << input1;
     temp.close();
     std::ifstream inputFile("temp1.xml");
-
-
-
     std::ofstream temp3("middle.xml");
     std::ofstream outputFile("output2.xml");
     temp3 << Minify2(inputFile, outputFile);
     temp3.close();
-
     std::ifstream temp2("middle.xml");
     outputFile << Parser(temp2, outputFile);
     temp2.close();
-
     XML_tree* T1 = new XML_tree();
-
     std::ifstream input("output2.xml");
     std::string output2="";
     output2+= "{" ;
     output2 += "\r\n";
     //output5 += "{" + '\n';
     tree(T1, input);
-
     Node* root = T1->get_root();
     Node* parent_of_root = new Node("dummy Node");
     root->parent = parent_of_root;
-
     output2+=XML_to_json(root);
     output2 += "\r\n";
     output2 +="}" ;
@@ -167,7 +152,6 @@ void MainWindow::on_pushButton_6_clicked()
     font.setFixedPitch(true);
     font.setPointSize(12);
     ui->plainTextEdit_2->setFont(font);
-
     QFontMetrics metrics(font);
     ui->plainTextEdit_2->setReadOnly(true);
     ui->plainTextEdit_2->setTabStopDistance(15);
@@ -183,7 +167,6 @@ void MainWindow::on_pushButton_2_clicked()
     std::string input1 = s2.toLocal8Bit().constData();
     std::vector<std::string> xmlfile ;
     xmlfile.push_back(input1);
-
     xml_corrector(xmlfile);
     std::stringstream strrr;
     for (std::vector<std::string>::const_iterator i = xmlfile.begin(); i != xmlfile.end(); ++i)
@@ -201,7 +184,6 @@ void MainWindow::on_pushButton_7_clicked()
     std::string input1 = s2.toLocal8Bit().constData();
     std::vector<std::string> xmlfile ;
     xmlfile.push_back(input1);
-
     std::vector<std::string> corrections = xml_corrector(xmlfile);
     std::string strrr;
     for (std::vector<std::string>::const_iterator i = corrections.begin(); i != corrections.end(); ++i)
@@ -211,3 +193,30 @@ void MainWindow::on_pushButton_7_clicked()
     ui->plainTextEdit_2->setPlainText(s3.toLocal8Bit());
 }
 
+//graph button
+void MainWindow::on_pushButton_9_clicked(){
+    std:: ofstream dotinput("test.dot");
+    QString s2 = ui->textBrowser->toPlainText();
+    std::string input1 = s2.toLocal8Bit().constData();
+    std::ofstream temp("temp1.xml");
+    temp << input1;
+    temp.close();
+    std::ifstream inputFile("temp1.xml");
+    std:: ofstream temp3("middle.xml");
+    std:: ofstream outputFile("output2.xml");
+    temp3 << Minify2(inputFile, outputFile);
+    temp3.close();
+    std:: ifstream temp2("middle.xml");
+    outputFile << Parser(temp2, outputFile);
+    temp2.close();
+    XML_tree* T1 = new XML_tree();
+    std:: ifstream input("output2.xml");
+    tree(T1, input);
+    Node* root = T1->get_root();
+    Graph(dotinput,root);
+    dotinput.close();
+    QDir::setCurrent("C:\\Users\\20112\\Downloads\\XML3");
+    system("dot -Tpng -O test.dot");
+    QPixmap p("C:\\Users\\20112\\Downloads\\XML3\\test.dot.png");
+    ui->label_2->setPixmap(p);
+}
